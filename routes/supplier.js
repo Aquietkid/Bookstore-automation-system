3ca7b8b25151123ce9ac6bc3ed3618c14ad19638
Ali Imran <93790085+Aquietkid@users.noreply.github.com>
2024-04-27 01:03:46 +0500

    Muhammad Umer fixed our code.

--- routes/supplier.js
+++ routes/supplier.js
@@ -1,38 +1,37 @@
 const express = require('express')
 const mysql = require('mysql')
 const router = express.Router()
+const connection = require("../config/persistence");
+
+// var connection = mysql.createConnection({
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASS,
+//     database: process.env.DB_DATABASE,
+// });
+
+
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log('DB Connected!');
+    var sql = 'SELECT * FROM Test_ali.Supplier;';
+    connection.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Result: " + result);
+    });
+
+    // res.send('Inside body of function');
+});
+
 
 // get book price
 
 router.get('/:id', (req, res) => {
-
-    var con = mysql.createConnection({
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_DATABASE,
-    });
-
-
-    con.connect(function (err) {
-        if (err) throw err;
-        console.log('DB Connected!');
-        var sql = 'SELECT * FROM Test_ali.Supplier;';
-        con.query(sql, function (err, result) {
-            if (err) throw err;
-            console.log("Result: " + result);
-        });
-        
-        res.send('Inside body of function');
-    });
+    
     res.send(req.params.id)
-
-    con.end((err) => {
-        if (err) return console.error(err.message);
-      
-        console.log('Close the database connection.');
-      });
+    
 
 
 })
@@ -42,7 +41,7 @@ router.get('/:id', (req, res) => {
 
 router.get('/', (req, res) => {
     res.send('Hey, I will return the price of the book')
-    
+
 })
 
 
--- server.js
+++ server.js
@@ -1,12 +1,36 @@
-const express = require('express')
-// const bodyParser = require("body-parser");
-const app = express()
-app.use(express.json())
+// const express = require('express')
+// // const bodyParser = require("body-parser");
+// const app = express()
+// app.use(express.json())
+
+
+
+// app.listen(20419, () => {
+//     console.log('Server is running on port 3000.');
+// });
+
+
+// const connection = require("./database");
+const express = require("express");
+const app = express();
+const router = express.Router();
+const cors = require("cors");
+
+// Middleware
+app.use(cors());
+app.use(express.json());
 
 
 const suppliersRouter = require('./routes/supplier')
+
+// Import and use routes
+app.use('/',router);
 app.use('/supplier', suppliersRouter)
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000.');
+const port = 3000; // Choose any available port you prefer
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
+
+
+module.exports = router;
