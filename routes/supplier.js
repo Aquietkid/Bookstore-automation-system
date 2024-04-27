faee83dfe29a2b9bc0a1ffd018ea94202682c84a
Ali Imran <93790085+Aquietkid@users.noreply.github.com>
2024-04-27 10:48:48 +0500

    Configured the DB to mae actual queries and process results accordingly.

--- routes/supplier.js
+++ routes/supplier.js
@@ -6,21 +6,58 @@ const connection = require("../config/persistence");
 
 
 
-// get book price
+// get item price
+
+/*
+    RESULT I WANT:
+    ItemName    SupplierName    Price
+    
+    PARAMS I GIVE: 
+    ItemID
+
+    QUERY:
+    SELECT Item.Name , Supplier.Name, SupplierItem.Price
+    FROM SupplierItem 
+    JOIN Supplier ON Supplier.ID = SupplierItem.SupplierID
+    JOIN Item ON Item.ID = SupplierItem.ItemID
+    WHERE ItemID = 2;
+    
+*/
 
 router.get('/:id', (req, res) => {
-
     connection.connect(function (err) {
         if (err) throw err;
+        const ItemID = req.params.id;
         // console.log('DB Connected!');
-        var sql = 'SELECT * FROM Supplier;';
-        connection.query(sql, function (err, result) {
+        var sql = 'SELECT Item.Name , Supplier.Name, SupplierItem.Price ' +
+        'FROM SupplierItem '+
+        'JOIN Supplier ON Supplier.ID = SupplierItem.SupplierID '+
+        'JOIN Item ON Item.ID = SupplierItem.ItemID '+
+        'WHERE ItemID = ?;';
+        connection.query(sql, [ItemID], function (err, result) {
             if (err) throw err;
-            console.log("Result: " + result);
+            if(result.length == 0) {
+                res.send('No Supplier ships that item');
+            }
+            if(result.length == 1) {
+                // If you only expect one result, you might want to send it directly
+                return res.json({data: result[0]});
+            } else {
+                // If you expect multiple results, you can send the array of results
+                var min = ({
+                    Name: result[0].Name, 
+                    Price: result[0].Price
+                })
+                result.forEach(element => {
+                    if(element.Price < min.Price){
+                        min.Name = element.Name;
+                        min.Price = element.Price;
+                    }
+                });
+                return res.json({data: min});
+            }
         });
-    
     });
-    res.send(req.params.id)
 })
 
 
