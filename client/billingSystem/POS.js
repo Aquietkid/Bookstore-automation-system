ae3bda4d11bfdce6fef6f3dbcf138790c6a7b873
Ali Imran <93790085+Aquietkid@users.noreply.github.com>
2024-04-29 16:52:23 +0500

    Changes to POS: fixed API of the POS page.

--- client/billingSystem/POS.js
+++ client/billingSystem/POS.js
@@ -2,13 +2,15 @@
 async function fetchItems() {
     try {
         //@Aquietkid fix the API: access inventory DB, not supplier
-        const response = await fetch('http://localhost:20419/supplier/all/items');
+        const response = await fetch('http://localhost:20419/inventory/all/items');
         const data = await response.json();
         const itemDropdown = document.getElementById('itemDropdown');
         data.forEach(item => {
             const option = document.createElement('option');
             option.value = item.ID; // Assuming each item has an ID
             option.textContent = item.Name; // Assuming each item has a name
+            // option.textContent = item.Price; // Assuming each item has a name
+            // option.textContent = item.Qty; // Assuming each item has a name
             itemDropdown.appendChild(option);
         });
     } catch (error) {
@@ -23,13 +25,14 @@ function addToCart() {
     const itemName = itemDropdown.options[itemDropdown.selectedIndex].text;
     const itemQty = document.getElementById('quantity').value;
 
+    const itemPrice = getItemPrice(selectedItem.ID).value;
     // Add selected item to the table
     const itemTableBody = document.getElementById('itemTableBody');
     const newRow = document.createElement('tr');
     newRow.innerHTML = `
         <td>${selectedItem}</td>
         <td>${itemName}</td>
-        <td>Price Placeholder</td>
+        <td>${itemPrice}</td>
         <td>${itemQty}</td>
     `;
     itemTableBody.appendChild(newRow);
@@ -39,5 +42,18 @@ function addToCart() {
 // window.onload = function() {
 //     fetchItems();
 // };
+
+
+async function getItemPrice(id) {
+    try {
+        //@Aquietkid fix the API: access inventory DB, not supplier
+        const response = await fetch(`http://localhost:20419/inventory/price/${id}`);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching items:', error);
+    }
+}
+
 
 window.addEventListener(onload, fetchItems());
